4e0dc9803d055406462286e08e3c10ac73a8062e
serdar-ozata <serdar1901@gmail.com>
2024-11-25 17:10:31 +0300

    total mem usage

--- main.c
+++ main.c
@@ -164,7 +164,9 @@ int main(int argc, char *argv[]) {
     int world_size, world_rank;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
+    if (world_rank == 0) {
+        printf("Starting...\n");
+    }
     ReaderRet parsedArgs = parseFileFromArgs(argc, argv);
     if (!parsedArgs.is_valid) {
         MPI_Finalize();
